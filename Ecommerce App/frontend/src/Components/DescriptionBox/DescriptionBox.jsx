92dc9f99f732cc19e80bfc6d2d0ead9a05f20020
Patrick Wright <patrick.wright92@hotmail.com>
2024-03-07 09:11:19 +1100

    further updates

---
+++ Ecommerce App/frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-nagivator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Description text goes here</p>
+        <p>Further Description text goes here</p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
